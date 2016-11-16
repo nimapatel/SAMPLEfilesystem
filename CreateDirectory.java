a718044df4f0fbb6fdc955c815a34f2bc94730a2
TPA003 <TAbraham611@yahoo.com>
2016-11-15 20:07:27 -0500

    Add files via upload

---
+++ CreateDirectory.java
@@ -0,0 +1,32 @@
+package OSProject;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class CreateDirectory {
+	  public static void main(String[] args) {
+		  	System.out.println("Name your Directory. Remember to start with C:\\Root\\");
+		  	Scanner s = new Scanner(System.in);
+		  	String Folder = "";
+	       	        
+		 
+		  		File files = new File(Folder=s.next());
+		  	
+	        if (!files.exists()) {
+	            if (files.mkdir()) {
+	                System.out.println("Directory is created!");
+	            } else {
+	                System.out.println("Directory already exists!");
+	            }
+	        }
+
+	        //loop needs to be added to do this as many times as user wants
+	       
+	        
+	       
+	        //after user ends the loop the user must pick if they are going to exit program or go back to main menu
+
+	    }
+
+
+	}
